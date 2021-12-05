c6332d7e04a7389b5375af26a5bb212a50dbed84
Edoardo GIACOMINI <egiacomi@e2r5p17.clusters.42paris.fr>
2021-12-05 18:24:50 +0100

    index added - easy way.2

--- moves_2.c
+++ moves_2.c
@@ -6,7 +6,7 @@
 /*   By: egiacomi <egiacomi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 18:52:16 by egiacomi          #+#    #+#             */
-/*   Updated: 2021/12/05 18:05:01 by egiacomi         ###   ########.fr       */
+/*   Updated: 2021/12/05 18:23:43 by egiacomi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	mv_reverse_rotate(t_stack *root, t_stack *mv, char s)
 	last_to_first = root->prev;
 	last_to_first->next = root->next;
 	root->next->prev = last_to_first;
-	ft_stkadd_prev(last_to_first, root, root);
+	ft_stkadd_prev(last_to_first, root);
 	if (s == 'a')
 		ft_stkadd_bot(mv, NULL, 0, "rra");
 	else
